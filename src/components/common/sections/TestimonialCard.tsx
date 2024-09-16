9f63d4cf1c40df2a761d117956e77f040f9c2d66
Piyushkashya <piyush8580kashyap@gmail.com>
2024-09-16 16:37:34 +0530

    minor fix

--- src/components/common/sections/TestimonialCard.tsx
+++ src/components/common/sections/TestimonialCard.tsx
@@ -5,31 +5,26 @@ import Parragraph from '../typography/Parragraph';
 const testimonials = [
   {
     name: 'Daniel and Aviel',
-    role: 'Frontend / Full Stack Developer for Long Term Work',
     content:
       "Wow. It's been an absolutely incredible experience working with Shubham. Beyond the fact that he is a top developer and true expert in his field, he is also a great person and extremely easy to work with. Our project required both creative and logical thinking, and has multiple levels of complexity. Throughout all stages of our project, Shubham was able to help us plan, research, and execute our vision at the highest level, with patience, and attention to even the smallest of details. In fact, we haven't stopped working with Shubham, we're continuing straight on to another project with him. Thanks a lot Shubham! Looking forward to what's next!",
   },
   {
     name: 'Daniel',
-    role: 'Full Stack Development',
     content:
-      "This was our second project working with Shubham, and just like the first, we had a fantastic experience working with him throughout the entire project! Both Shubham's skills and personality fit in perfectly with our company's culture and vision, and we're excited to continue working together on our new projects. Thanks Shubham!",
+          "This was our second project working with Shubham, and just like the first, we had a fantastic experience working with him throughout the entire project! Both Shubham's skills and personality fit in perfectly with our company's culture and vision, and we're excited to continue working together on our new projects. Thanks Shubham!",
   },
   {
     name: 'Tobias',
-    role: 'Fix collection implementation according to specifications',
     content:
       'This is the second time we worked with Shubham. We needed someone to rework and tidy up a sloppily implemented configuration. His excellent communication skills, prompt responses, and attention to detail make working with him a charm. He kept us up-to-date regularly, implemented feedback quickly, and completed the task in an appropriate time. We are looking forward to working with Shubham again.',
   },
   {
-    name: 'Arturo',
-    role: 'Wordpress Developer With Elementor Experince ',
+    name: 'Utkarsh S.',
     content:
       "The freelancer was very honest and has a good personality and I liked his dedication to his work. Have a good knowledge of Elementor & WordPress and helped me optimize my website's loading time. The Freelancer is very much experienced and I would like to work with him again on bigger project. Highly Recommended!!",
   },
   {
     name: 'Ismail',
-    role: 'Nextjs mantine developer frontend',
     content:
       'Shubham paid attention to details and worked on my startup diligently. I recommend',
   },
@@ -46,8 +41,7 @@ const TestimonialsSection = () => {
         {testimonials.map((testimonial, index) => (
           <div
             key={index}
-            className="flex flex-col rounded-lg  border-2 bg-white p-6 shadow-lg  transition-shadow hover:shadow-xl  dark:border-primary-800 dark:bg-primary-700"
-            style={{ boxShadow: '0px 0px 10px 4px rgba(255,255,255,0.3)' }}
+            className="flex flex-col rounded-lg  border-2 bg-white p-6 shadow-lg  transition-shadow hover:shadow-xl  dark:border-slate-300 dark:bg-primary-700"
           >
             <blockquote className="mt-4 grow">
               <p className="w-full text-lg text-black dark:text-slate-200">{`${testimonial.content}`}</p>
@@ -61,9 +55,6 @@ const TestimonialsSection = () => {
               <div className="ml-4">
                 <p className="font-semibold  text-black dark:text-slate-200">
                   {testimonial.name}
-                </p>
-                <p className="text-sm text-black dark:text-slate-200">
-                  {testimonial.role}
                 </p>
               </div>
             </div>
